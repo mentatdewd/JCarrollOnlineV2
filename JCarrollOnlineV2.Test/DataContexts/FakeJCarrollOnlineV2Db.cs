44d3661f9fa100860fce390a969c9a20111b66a8
mentatdewd <mentatdewd@comcast.net>
2015-04-24 15:25:36 -0700

    Forgot to add FakeJCarrollOnlineV2Db.cs

---
+++ JCarrollOnlineV2.Test/DataContexts/FakeJCarrollOnlineV2Db.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JCarrollOnlineV2.Test.DataContexts
+{
+    class FakeJCarrollOnlineV2Db<TEntity> : DbSet<TEntity>, IQueryable, IEnumerable<TEntity>, IDbAsyncEnumerable<TEntity>
+        where TEntity : class
+    {
+        ObservableCollection<TEntity> _data;
+        IQueryable _query;
+
+        public FakeJCarrollOnlineV2Db()
+        {
+            _data = new ObservableCollection<TEntity>();
+            _query = _data.AsQueryable();
+        }
+
+        public override TEntity Add(TEntity item)
+        {
+            _data.Add(item);
+            return item;
+        }
+
+        public override TEntity Remove(TEntity item)
+        {
+            _data.Remove(item);
+            return item;
+        }
+
+        public override TEntity Attach(TEntity item)
+        {
+            _data.Add(item);
+            return item;
+        }
+
+        public override TEntity Create()
+        {
+            return Activator.CreateInstance<TEntity>();
+        }
+
+        public override TDerivedEntity Create<TDerivedEntity>()
+        {
+            return Activator.CreateInstance<TDerivedEntity>();
+        }
+
+        public override ObservableCollection<TEntity> Local
+        {
+            get { return _data; }
+        }
+
+        Type IQueryable.ElementType
+        {
+            get { return _query.ElementType; }
+        }
+
+        Expression IQueryable.Expression
+        {
+            get { return _query.Expression; }
+        }
+
+        IQueryProvider IQueryable.Provider
+        {
+            get { return new FakeJCarrollOnlineV2DbAsyncQueryProvider<TEntity>(_query.Provider); }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IEnumerator<TEntity> IEnumerable<TEntity>.GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IDbAsyncEnumerator<TEntity> IDbAsyncEnumerable<TEntity>.GetAsyncEnumerator()
+        {
+            return new FakeJCarrollOnlineV2DbAsyncEnumerator<TEntity>(_data.GetEnumerator());
+        }
+    }
+
+    internal class FakeJCarrollOnlineV2DbAsyncQueryProvider<TEntity> : IDbAsyncQueryProvider
+    {
+        private readonly IQueryProvider _inner;
+
+        internal FakeJCarrollOnlineV2DbAsyncQueryProvider(IQueryProvider inner)
+        {
+            _inner = inner;
+        }
+
+        public IQueryable CreateQuery(Expression expression)
+        {
+            return new FakeJCarrollOnlineV2DbAsyncEnumerable<TEntity>(expression);
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            return new FakeJCarrollOnlineV2DbAsyncEnumerable<TElement>(expression);
+        }
+
+        public object Execute(Expression expression)
+        {
+            return _inner.Execute(expression);
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            return _inner.Execute<TResult>(expression);
+        }
+
+        public Task<object> ExecuteAsync(Expression expression, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute(expression));
+        }
+
+        public Task<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute<TResult>(expression));
+        }
+    }
+
+    internal class FakeJCarrollOnlineV2DbAsyncEnumerable<T> : EnumerableQuery<T>, IDbAsyncEnumerable<T>, IQueryable<T>
+    {
+        public FakeJCarrollOnlineV2DbAsyncEnumerable(IEnumerable<T> enumerable)
+            : base(enumerable)
+        { }
+
+        public FakeJCarrollOnlineV2DbAsyncEnumerable(Expression expression)
+            : base(expression)
+        { }
+
+        public IDbAsyncEnumerator<T> GetAsyncEnumerator()
+        {
+            return new FakeJCarrollOnlineV2DbAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+        }
+
+        IDbAsyncEnumerator IDbAsyncEnumerable.GetAsyncEnumerator()
+        {
+            return GetAsyncEnumerator();
+        }
+
+        IQueryProvider IQueryable.Provider
+        {
+            get { return new FakeJCarrollOnlineV2DbAsyncQueryProvider<T>(this); }
+        }
+    }
+
+    internal class FakeJCarrollOnlineV2DbAsyncEnumerator<T> : IDbAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _inner;
+
+        public FakeJCarrollOnlineV2DbAsyncEnumerator(IEnumerator<T> inner)
+        {
+            _inner = inner;
+        }
+
+        public void Dispose()
+        {
+            _inner.Dispose();
+        }
+
+        public Task<bool> MoveNextAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_inner.MoveNext());
+        }
+
+        public T Current
+        {
+            get { return _inner.Current; }
+        }
+
+        object IDbAsyncEnumerator.Current
+        {
+            get { return Current; }
+        }
+    }
+}
