e00acf73e6b6a4bce9c22e602fcdf66dd6842ea0
mentatdewd <mentatdewd@comcast.net>
2015-04-25 15:23:15 -0700

    Another attempt at fixing the access to the users page

--- JCarrollOnlineV2/Global.asax.cs
+++ JCarrollOnlineV2/Global.asax.cs
@@ -1,17 +1,13 @@
 ﻿using JCarrollOnlineV2.DataContexts;
-using JCarrollOnlineV2.Entities;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Threading;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.Security;
+using WebMatrix.WebData;
 
 namespace JCarrollOnlineV2
 {
@@ -41,7 +37,38 @@ namespace JCarrollOnlineV2
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            // Ensure ASP.NET Simple Membership is initialized only once per app start
+            LazyInitializer.EnsureInitialized(ref _initializer, ref _isInitialized, ref _initializerLock);
             //AddRoleAndUser();
+        }
+        private static SimpleMembershipInitializer _initializer;
+        private static object _initializerLock = new object();
+        private static bool _isInitialized;
+
+        private class SimpleMembershipInitializer
+        {
+            public SimpleMembershipInitializer()
+            {
+                Database.SetInitializer<JCarrollOnlineV2Db>(null);
+
+                try
+                {
+                    using (var context = new JCarrollOnlineV2Db())
+                    {
+                        if (!context.Database.Exists())
+                        {
+                            // Create the SimpleMembership database without Entity Framework migration schema
+                            ((IObjectContextAdapter)context).ObjectContext.CreateDatabase();
+                        }
+                    }
+
+                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidOperationException("The ASP.NET Simple Membership database could not be initialized. For more information, please see http://go.microsoft.com/fwlink/?LinkId=256588", ex);
+                }
+            }
         }
     }
 }
