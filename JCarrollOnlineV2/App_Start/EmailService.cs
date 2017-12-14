2ac7f420ed70f02e6b6c61a9ad19735a9901a3df
John Carroll <john.carroll@happay.io>
2017-12-13 20:26:47 -0800

    enabled email

--- JCarrollOnlineV2/App_Start/EmailService.cs
+++ JCarrollOnlineV2/App_Start/EmailService.cs
@@ -15,7 +15,7 @@ namespace JCarrollOnlineV2
 
             using (var smtpClient = new SmtpClient())
             {
-                //await smtpClient.SendMailAsync(mailMessage);
+                await smtpClient.SendMailAsync(mailMessage);
             }
         }
 
