13f876fb22164f336baa3022f209d2e3120afa9e
John Carroll <john.carroll@happay.io>
2017-12-15 08:11:28 -0800

    formatting welcome page

---
+++ JCarrollOnlineV2.ViewModels/Home/LatestForumThreadItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace JCarrollOnlineV2.ViewModels
+{
+    public class LatestForumThreadItemViewModel : ViewModelBase
+    {
+        public string ForumTitle { get; set; }
+        public string ThreadTitle { get; set; }
+
+        public int ForumId { get; set; }
+
+        public int ThreadId { get; set; }
+    }
+}
